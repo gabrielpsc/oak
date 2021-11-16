bdf2b5c1953a577fe10f41810beb626addbcb254
gabrielpsc <64293697+gabrielpsc@users.noreply.github.com>
2021-11-16 17:46:09 -0300

    Update Demo_RM.tf

--- Demo_RM.tf
+++ Demo_RM.tf
@@ -11,7 +11,7 @@ provider "oci" {
 
 resource "oci_core_vcn" "vcn1" {
   cidr_block     = "172.20.0.0/16"
-  dns_label      = "RM Webinar"
+  dns_label      = "rmwebinar"
   compartment_id = "${var.compartment_ocid}"
   display_name   = "RM Webinar"
 }
